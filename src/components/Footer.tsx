6cba6f5d2859bc6fb274cd6d9ff7643daeb59fdb
Kong670610723 <Phurin0946122606@gmail.com>
2025-09-29 06:46:43 +0700

    practice02

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+//Footer.tsx
+import { Text, Group } from "@mantine/core";
+export default function Footer(/* add props here */) {
+  return (
+    <Group p="md" justify="center">
+      <Text>
+        © CPE207-2025 Phurin Inthajak 670610723. All rights reserved.
+      </Text>
+    </Group>
+  );
+}
